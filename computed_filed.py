43debe56e50bb621b9bc89672f1c9ad0c6b8e0fe
Parveen Saroha <parveensaroha2002@gmail.com>
2025-07-08 17:44:32 +0530

    code updated

--- computed_filed.py
+++ computed_filed.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel,EmailStr,  Field, field_validator, model_validator
+from pydantic import BaseModel,EmailStr,  Field, computed_field
 from typing import Optional, List, Dict, Annotated
 
 
@@ -9,15 +9,20 @@ class Patient(BaseModel):
     height: Optional[float] = None
     weight: Optional[float] = Field(default=None, ge=0, description="Weight in kilograms, must be non-negative") 
     contact_info: Optional[Dict[str, str]] = None
+    bmi = Optional[float] = None
     
-
+    @computed_field
+    @property
+    def calculate_bmi(self) -> float:
+        if self.height and self.weight:
+            return round(self.weight / (self.height ** 2), 2)
 
 # Example usage of the Patient model
 def insert_patient(patient: Patient):
     # Here you would typically insert the patient into a database or perform some action
-    print(f"Inserting patient: {patient.name}, Age: {patient.age}, Email: {patient.email}, Height: {patient.height}, Weight: {patient.weight}, Contact: {patient.contact_info}" )
+    print(f"Inserting patient: {patient.name}, Age: {patient.age}, Email: {patient.email}, Height: {patient.height}, Weight: {patient.weight}, Contact: {patient.contact_info}, BMI : {patient.calculate_bmi}") 
 
-patient_info = { 'name': 'Parveen', 'age': 20, 'email': 'pks@iitm.com', 'height': 5.5, 'weight': 60.0,  'contact_info': {'phone': '123-456-7890', 'email':'abbb@iitm.com'} }
+patient_info = { 'name': 'Parveen', 'age': 20, 'email': 'pks@iitm.com', 'height': 1.8, 'weight': 60.0,  'contact_info': {'phone': '123-456-7890', 'email':'abbb@iitm.com'} }
 
 pateint1 = Patient(**patient_info)
 insert_patient(pateint1)
