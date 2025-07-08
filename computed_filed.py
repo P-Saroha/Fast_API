f7ee5edca6ea8e6dfadd53feb5bf742d98d21573
Parveen Saroha <parveensaroha2002@gmail.com>
2025-07-08 17:33:59 +0530

    code updated

---
+++ computed_filed.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel,EmailStr,  Field, field_validator, model_validator
+from typing import Optional, List, Dict, Annotated
+
+
+class Patient(BaseModel):
+    name: Annotated[str, Field(min_length=1, max_length=100,title = "pateint name", description="Full name of the patient")]
+    age: int
+    email: EmailStr
+    height: Optional[float] = None
+    weight: Optional[float] = Field(default=None, ge=0, description="Weight in kilograms, must be non-negative") 
+    contact_info: Optional[Dict[str, str]] = None
+    
+
+
+# Example usage of the Patient model
+def insert_patient(patient: Patient):
+    # Here you would typically insert the patient into a database or perform some action
+    print(f"Inserting patient: {patient.name}, Age: {patient.age}, Email: {patient.email}, Height: {patient.height}, Weight: {patient.weight}, Contact: {patient.contact_info}" )
+
+patient_info = { 'name': 'Parveen', 'age': 20, 'email': 'pks@iitm.com', 'height': 5.5, 'weight': 60.0,  'contact_info': {'phone': '123-456-7890', 'email':'abbb@iitm.com'} }
+
+pateint1 = Patient(**patient_info)
+insert_patient(pateint1)
+
